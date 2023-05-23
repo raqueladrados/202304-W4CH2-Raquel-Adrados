ad410816f2b2faacb338694ab746b0a211543438
Raquel Adrados <radrados00@gmail.com>
2023-05-23 22:24:40 +0200

    Change type personal data and add structure home

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,18 +1,34 @@
-import { PersonalData } from "../../models/personal.data";
+import { useState } from "react";
 export function Home() {
+  const [PersonalData, setPersonalData] = useState({
+    name: "",
+    lastName: "",
+    birthday: "",
+    gender: "male | female | other | prefer not to mention",
+    email: "",
+    checkbox: "",
+  });
   return (
     <>
       <section>
         <form>
-          <label htmlFor="name"></label>
+          <label htmlFor="name">{PersonalData.name}</label>
           <input type="text" name="name" id="name" />
-          <label htmlFor="lastname"></label>
+          <label htmlFor="lastname">{PersonalData.name}</label>
           <input type="text" name="lastname" id="lastname" />
-          <label htmlFor="birthday"></label>
+          <label htmlFor="birthday">{PersonalData.birthday}</label>
           <input type="date" id="birthday" name="birthday" value="" />
           <fieldset>
-            <legend></legend>
-            <input type="radio" id="" name="gender" />
+            <legend>Select your gender:</legend>
+            <input type="radio" id="name" name="gender" value="male" />
+            <input type="radio" id="name" name="gender" value="female" />
+            <input type="radio" id="name" name="gender" value="other" />
+            <input
+              type="radio"
+              id="name"
+              name="gender"
+              value="prefer not to mention"
+            />
           </fieldset>
         </form>
       </section>
