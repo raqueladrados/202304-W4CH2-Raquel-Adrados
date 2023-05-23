63cd867f65b5513f87ecd70692d270c460526150
Raquel Adrados <radrados00@gmail.com>
2023-05-23 19:59:26 +0200

    Add form in home and delete home html file

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,10 +1,12 @@
 import { Header } from "../header/Header";
+import { Home } from "../home/Home";
 import "./App.css";
 
 export function App() {
   return (
     <>
       <Header></Header>
+      <Home></Home>
     </>
   );
 }
--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,3 +1,21 @@
+import { PersonalData } from "../../models/personal.data";
 export function Home() {
-  return <></>;
+  return (
+    <>
+      <section>
+        <form>
+          <label htmlFor="name"></label>
+          <input type="text" name="name" id="name" />
+          <label htmlFor="lastname"></label>
+          <input type="text" name="lastname" id="lastname" />
+          <label htmlFor="birthday"></label>
+          <input type="date" id="birthday" name="birthday" value="" />
+          <fieldset>
+            <legend></legend>
+            <input type="radio" id="" name="gender" />
+          </fieldset>
+        </form>
+      </section>
+    </>
+  );
 }
